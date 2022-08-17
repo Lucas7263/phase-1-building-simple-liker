087b493c869363ceec46703c48d62ec78fed90e5
Lucas <lucaswoodyard@yahoo.com>
2022-08-17 16:32:58 -0500

    commit tree 8f47865d2deaee99cd24e9ecc2add2629b09d67a

--- main.js
+++ main.js
@@ -3,7 +3,57 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+ 
+  
+   document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('#modal').hidden = true;
+   
+  })
+  console.log(modal)
 
+
+  function toggleHeart() {
+     //Error being popping up on successful server call
+     
+    } 
+  
+
+  function showError() {
+    document.getElementById('modal').hidden = false
+    // makes error message appear AFTER 3 seconds
+    //setTimeout(() => document.getElementById('modal').hidden = false, 3000)
+    setTimeout(() => document.getElementById('#modal').hidden = true, 3000)
+  }
+
+  function like() {
+    document.addEventListener('click', () => {
+      //const like = document.getElementById('like-glyph')
+      document.querySelector('.activated-heart')
+    //document.getElementsByClassName("like-glyph") 
+    
+    let p1 = mimicServerCall()
+    p1.then(function(resolve) {
+      console.log('Okay!')
+      toggleHeart();
+      
+      
+     
+    }).catch(function(e) {
+      showError();
+      
+    })
+    
+    
+      
+    })
+  }    
+    like()
+
+    // function toggleHeart() {
+    //   document.getElementsByClassName("like-glyph") = FULL_HEART
+    // }
+
+  
 
 
 
